adb5e14324ca79a25f6a86ca2adfd505351e694b
hamlet <haml262@hotmail.com>
2023-06-23 00:42:36 -0400

    modify the code

--- script.js
+++ script.js
@@ -1,7 +1,33 @@
 // script.js
 function generateRandomColor() {
-    var randomColor = '#' + Math.floor(Math.random() * 16777215).toString(16);
-    var colorDisplay = document.getElementById('colorDisplay');
-    colorDisplay.style.backgroundColor = randomColor;
-  }
+  var randomColor = getRandomColor();
+  var colorDisplay = document.getElementById('colorDisplay');
+  var hexCode = document.getElementById('hexCode');
+  var rgbCode = document.getElementById('rgbCode');
   
+  colorDisplay.style.backgroundColor = randomColor;
+  hexCode.textContent = 'Hex: ' + randomColor;
+  rgbCode.textContent = 'RGB: ' + convertHexToRgb(randomColor);
+}
+
+function getRandomColor() {
+  var letters = '0123456789ABCDEF';
+  var color = '#';
+  for (var i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
+
+function convertHexToRgb(hex) {
+  var shorthandRegex = /^#?([a-f\d])([a-f\d])([a-f\d])$/i;
+  hex = hex.replace(shorthandRegex, function(m, r, g, b) {
+    return r + r + g + g + b + b;
+  });
+  var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+  var rgb = [];
+  rgb.push(parseInt(result[1], 16));
+  rgb.push(parseInt(result[2], 16));
+  rgb.push(parseInt(result[3], 16));
+  return rgb.join(', ');
+}
